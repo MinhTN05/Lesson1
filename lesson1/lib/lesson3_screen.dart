ac04982b3694222dc4e39b1e24e0bb9e44accdad
MinhTN05 <trannhatminh460@gmail.com>
2024-05-13 14:00:36 +0700

    Add hình đẹp vl

--- lesson1/lib/lesson3_screen.dart
+++ lesson1/lib/lesson3_screen.dart
@@ -1,30 +1,26 @@
-
-
 import 'package:flutter/material.dart';
 
-void main () {
-  int solanbannut = 0;
+void main() {
   runApp(
     MaterialApp(
+      // xóa debug ở góc phải trên màn hình
+      debugShowCheckedModeBanner: false,
       title: 'My App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My Test Home Page'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Day la Page 2'),
-              ElevatedButton(
-                  onPressed: () {
-                    solanbannut++;
-                    print("Nut da duoc bam! $solanbannut");
-              }, child: Text("Click me!!!"))
-            ],
-          ),
-        ),
-      ),
-    )
+      home: buildHomeScreen(),
+    ),
+  );
+}
+
+Widget buildHomeScreen() {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('My Test Home Page'),
+    ),
+    body: Column(
+      children: [
+        // Text('Home Screen'),
+        Image.network('https://i.pinimg.com/564x/5e/04/9e/5e049e87cc382991a8420494c54ad5e8.jpg'),
+      ],
+    ),
   );
 }
