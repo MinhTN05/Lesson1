d81b49ddd2568b50fd4e01877d11abf9b2861a1c
MinhTN05 <trannhatminh460@gmail.com>
2024-05-13 16:19:47 +0700

    Chuyển trang qua lại với nhau

--- lesson1/lib/list_view_screen.dart
+++ lesson1/lib/list_view_screen.dart
@@ -9,30 +9,40 @@ class ListViewScreen extends StatelessWidget {
         title: const Text('ListView Screen'),
       ),
       body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.all(8),
-          children: <Widget>[
+        child: Column(
+          children: [
             Container(
-              height: 50,
-              color: Colors.amber[600],
-              child: const Center(
-                child: Text('Entry A'),
+              height: 150,
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
+                  Container(
+                    height: 50,
+                    color: Colors.amber[600],
+                    child: const Center(
+                      child: Text('Entry A'),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.amber[500],
+                    child: const Center(
+                      child: Text('Entry B'),
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    color: Colors.amber[100],
+                    child: const Center(
+                      child: Text('Entry C'),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Container(
-              height: 50,
-              color: Colors.amber[500],
-              child: const Center(
-                child: Text('Entry B'),
-              ),
-            ),
-            Container(
-              height: 50,
-              color: Colors.amber[100],
-              child: const Center(
-                child: Text('Entry C'),
-              ),
-            ),
+            ElevatedButton(onPressed: () {
+              Navigator.pop(context);
+            }, child: Text('Back to Screen 1')),
           ],
         ),
       ),
