1bbb8a0ad62c91d5543921c0a9cff70f4bb26939
MinhTN05 <trannhatminh460@gmail.com>
2024-05-15 11:37:04 +0700

    add listview

--- lesson1/lib/View/component/lesson5.dart
+++ lesson1/lib/View/component/lesson5.dart
@@ -7,35 +7,63 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Lesson 5',
+      title: 'Anime',
       home: MyPage(),
     );
   }
 }
 
 class MyPage extends StatelessWidget {
+  late final List<String> items;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    items = List<String>.generate(50, (i) => 'Xem anime $i');
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Lesson 5',
+          'Anime',
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        child: Center(
-          child: Image.asset(
-            'assets/images/7fec547d3477854efda2b76952fef53c.jpg',
-            width: 300,
-            height: 300,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset(
+                'assets/images/7fec547d3477854efda2b76952fef53c.jpg',
+                width: 250,
+                height: 250,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(items[index]),
+                      onTap: () {
+                        String mess = items[index];
+
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(SnackBar(content: Text(mess)));
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
