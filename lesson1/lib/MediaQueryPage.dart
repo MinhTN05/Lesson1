3f9918cb652340517c7f82b24bffa17dbf266b1b
MinhTN05 <trannhatminh460@gmail.com>
2024-05-17 12:12:50 +0700

    add Media_Query này giúp cho xem các thông số từ thiết bị truyền vào

---
+++ lesson1/lib/MediaQueryPage.dart
@@ -0,0 +1,85 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'MediaQueryPage';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MediaQueryPage(),
+    );
+  }
+}
+
+class MediaQueryPage extends StatelessWidget {
+  const MediaQueryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MediaQueryPage'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const ScreenSize(),
+            Image.asset(
+              'assets/images/depvl.jpg',
+              width: 300,
+              height: 300,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ScreenSize extends StatelessWidget {
+  const ScreenSize({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    bool isAndroid = Platform.isAndroid;
+    bool isiOS = Platform.isIOS;
+    var op = Platform.operatingSystem;
+    var opVer = Platform.operatingSystemVersion;
+    var b = Platform.environment;
+
+    var mediaQuery = MediaQuery.of(context);
+    var screenSize = mediaQuery.size;
+    var orientation = mediaQuery.orientation;
+    var fontScaling = mediaQuery.textScaleFactor;
+    var padding = mediaQuery.padding;
+    var deviceModel = mediaQuery.platformBrightness;
+    var pixelRatio = mediaQuery.devicePixelRatio;
+    var disableAnimation = mediaQuery.disableAnimations;
+    var platformBrightness = mediaQuery.platformBrightness;
+
+    return Container(
+      child: Text(
+        '''width = ${(screenSize.width).toStringAsFixed(3)}\n
+height = ${(screenSize.height).toStringAsFixed(3)}\n
+orientation = ${orientation}\n
+textScaleFactor = ${fontScaling}\n
+padding = ${padding}\n
+disableAnimation = ${disableAnimation}\n
+platformBrightness = ${platformBrightness}\n
+''',
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
