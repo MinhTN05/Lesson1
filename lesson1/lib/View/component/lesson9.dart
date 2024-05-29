d208c8830e32e7f1c649e1ccc43986d2b6f38811
MinhTN05 <trannhatminh460@gmail.com>
2024-05-29 17:47:48 +0700

    lesson 9: BuildContext

--- lesson1/lib/View/component/lesson9.dart
+++ lesson1/lib/View/component/lesson9.dart
@@ -16,22 +16,58 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Builder(
-          builder: (BuildContext context) {
-            return TextButton(
-              child: Text('show snackbar'),
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.pink,
-                primary: Colors.white, // Màu chữ
-              ),
-              onPressed: () {
-                final snackBar = SnackBar(content: Text('Nội dung gì đó!'));
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Builder(
+              builder: (BuildContext context) {
+                return TextButton(
+                  child: Text('show snackbar'),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.pink,
+                    primary: Colors.white, // Màu chữ
+                  ),
+                  onPressed: () {
+                    final snackBar = SnackBar(content: Text('Nội dung gì đó!'));
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  },
+                );
               },
-            );
-          },
+            ),
+            FrogColor(
+              color: Colors.green,
+              child: Builder(
+                builder: (BuildContext innerContext) {
+                  return Text(
+                    'Hello Frog',
+                    style: TextStyle(color: FrogColor.of(innerContext).color),
+                  );
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+}
+
+class FrogColor extends InheritedWidget {
+  const FrogColor({
+    Key? key,
+    required this.color,
+    required Widget child,
+  }) : super(key: key, child: child);
+
+  final Color color;
+
+  static FrogColor of(BuildContext context) {
+    final FrogColor? result =
+        context.dependOnInheritedWidgetOfExactType<FrogColor>();
+    assert(result != null, 'No FrogColor found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(FrogColor old) => color != old.color;
 }
