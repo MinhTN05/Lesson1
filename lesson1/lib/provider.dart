6f2c65722553d9569e53448b6382933056c7fd55
MinhTN05 <trannhatminh460@gmail.com>
2024-05-30 11:33:02 +0700

    thêm provider

---
+++ lesson1/lib/provider.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var myData = MyModel();
+
+    // return Provider<MyModel>(
+    //   create: (context) => myData,
+    //   child: (),
+    // );
+
+    return Provider<MyModel>(
+      create: (BuildContext context) {
+        return myData;
+      },
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: const Text('My App')),
+          backgroundColor: Colors.grey,
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(20),
+                color: Colors.green[200],
+                child: Consumer<MyModel>(
+                  builder: (context, mymodel, child) {
+                    return ElevatedButton(
+                      child: const Text('Do something'),
+                      onPressed: () {
+                        mymodel.doSomething();
+                      },
+                    );
+                  },
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(35),
+                color: Colors.blue[200],
+                child: Consumer<MyModel>(
+                  builder: (context, mymodel, child) {
+                    return Text(mymodel.text);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyModel {
+  String text = "Hello";
+
+  void doSomething() {
+    text = "World";
+  }
+}
