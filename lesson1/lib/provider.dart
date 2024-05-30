203d223af83aa9a76e7e22d4abd7b1dd3dbbe71e
MinhTN05 <trannhatminh460@gmail.com>
2024-05-30 11:40:49 +0700

    thêm provider

--- lesson1/lib/provider.dart
+++ lesson1/lib/provider.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+void main () {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
